3563e217d4de5c408e38d2716ce2076746bf337f
Alex <akelizarovav@gmail.com>
2023-02-26 21:08:21 +0300

    Added data for performing checks: invalid login, order completion

--- src/test/java/readProperties/ConfigProvider.java
+++ src/test/java/readProperties/ConfigProvider.java
@@ -4,15 +4,25 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 /**
- * Интерфейс для взаимодействия с файлом application.conf
+ * Interface for interacting with the application.conf file
  */
 public interface ConfigProvider {
-
     static Config readConfig() {
         return ConfigFactory.load("application.conf");
     }
-
+    //url to run the test
     String URL = readConfig().getString("url");
+    //valid login credentials
     String USER_LOGIN = readConfig().getString("userParams.user.login");
     String USER_PASSWORD = readConfig().getString("userParams.user.password");
+    //invalid login credentials
+    String INVALID_USER_LOGIN = readConfig().getString("userParams.invalidUser.login");
+    String INVALID_USER_PASSWORD = readConfig().getString("userParams.invalidUser.password");
+    //valid credentials for order verification
+    String FIRST_NAME = readConfig().getString("dataForChecks.checkoutYourInformation.firstName");
+    String LAST_NAME = readConfig().getString("dataForChecks.checkoutYourInformation.lastName");
+    String POSTAL_CODE = readConfig().getString("dataForChecks.checkoutYourInformation.postalCod");
+    //data for order completion checks
+    String URL_COMPLETED_ORDER = readConfig().getString("dataForChecks.checkout.urlCompletedPage");
+    String MESSAGE_COMPLETED_ORDER = readConfig().getString("dataForChecks.checkout.messageCompletedPage");
 }
